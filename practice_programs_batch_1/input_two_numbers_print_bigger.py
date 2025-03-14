cc51183d32bc7ecf8e56203e20db58505e29a93a
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 17:57:06 +0800

    code to determine which of the two numbers are bigger

--- practice_programs_batch_1/input_two_numbers_print_bigger.py
+++ practice_programs_batch_1/input_two_numbers_print_bigger.py
@@ -2,3 +2,7 @@
 first_number=int(input("Enter first number:"))
 second_number=int(input("Enter second number:"))
 #Determine which of the two numbers are bigger
+if first_number>second_number:
+    print("The bigger number is", first_number)
+elif first_number<second_number:
+    print("The bigger number is", second_number)
