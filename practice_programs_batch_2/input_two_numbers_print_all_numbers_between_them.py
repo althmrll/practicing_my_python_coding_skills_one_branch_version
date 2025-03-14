6991ac109868b87007ec2019df01777af936d050
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:32:47 +0800

    used while loop to continue printing until the value does not reach the higher_number

--- practice_programs_batch_2/input_two_numbers_print_all_numbers_between_them.py
+++ practice_programs_batch_2/input_two_numbers_print_all_numbers_between_them.py
@@ -1,7 +1,13 @@
+global middle_number
+ 
 #ask user to input numbers
 lower_number= int(input("Enter lower limit:"))
 higher_number= int(input("Enter higher limit:"))
  
 #Add one to lower number of the two(lowernumber+1)
+middle_number= lower_number+1
+ 
 #continue adding one until it reaches just below the higher number befre stopping
-#print the numbers between them
+while lower_number<higher_number:
+    print(middle_number) #print the numbers between them
+    middle_number=middle_number+1
