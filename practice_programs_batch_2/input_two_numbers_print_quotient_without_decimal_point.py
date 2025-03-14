2f1b3c4f6dd246acba8214deb9a9f3ef5266ddc9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 20:21:52 +0800

    wrote code so user can input the two numbers

--- practice_programs_batch_2/input_two_numbers_print_quotient_without_decimal_point.py
+++ practice_programs_batch_2/input_two_numbers_print_quotient_without_decimal_point.py
@@ -1,3 +1,6 @@
 #Ask user to input two numbers
+first_number= int(input("Enter first numebr:"))
+second_number= int(input("Enter second number:"))
+ 
 #Find their quotient (1st/second)
 #print their result
