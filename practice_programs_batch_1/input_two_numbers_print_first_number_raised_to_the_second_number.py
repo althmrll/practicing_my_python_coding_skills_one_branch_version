1872ee1e68c2265124dba4aa770775f2ba89549f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:38:28 +0800

    equation to raise the first number to the second number

--- practice_programs_batch_1/input_two_numbers_print_first_number_raised_to_the_second_number.py
+++ practice_programs_batch_1/input_two_numbers_print_first_number_raised_to_the_second_number.py
@@ -3,4 +3,6 @@ first_number = int(input("Enter first number:"))
 second_number = int(input("Enter second number:"))
 
 #Raise first number to second number
+result = first_number^second_number
+
 #Print result
