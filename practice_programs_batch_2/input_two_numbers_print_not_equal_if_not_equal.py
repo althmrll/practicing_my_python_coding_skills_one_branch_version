45e74a0560f0369d2f031d0be9a6e3492c9a5027
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 19:54:45 +0800

    wrote code for conditions and outputs for when the number is equal and not equal

--- practice_programs_batch_2/input_two_numbers_print_not_equal_if_not_equal.py
+++ practice_programs_batch_2/input_two_numbers_print_not_equal_if_not_equal.py
@@ -3,4 +3,9 @@ first_number= int(input("Enter first number:"))
 second_number= int(input("Enter second numebr:"))
  
 #determine whether the two numbers ar enot equal
+if first_number!=second_number:
+    print("NOT equal")
+else:
+    print("Equal")
+     
 #print "not equal" if not equal
