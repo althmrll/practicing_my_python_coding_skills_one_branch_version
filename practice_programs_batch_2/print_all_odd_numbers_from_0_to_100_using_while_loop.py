348703635185e975784252fbfe946ed39b462293
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:11:16 +0800

    wrote the condition to tell apart the odd numbers from the even numbers

--- practice_programs_batch_2/print_all_odd_numbers_from_0_to_100_using_while_loop.py
+++ practice_programs_batch_2/print_all_odd_numbers_from_0_to_100_using_while_loop.py
@@ -1,7 +1,9 @@
 global number
  
- #print all odd numbers from 0 to 100 using while loop
- number=0
+#print all odd numbers from 0 to 100 using while loop
+number=0
  
- while number!=100:
-     number=number+1
+while number!=100:
+    number=number+1
+    if number%2==1:
+        print(number)
