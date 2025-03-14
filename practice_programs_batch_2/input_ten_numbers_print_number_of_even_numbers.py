f9414ce5d966ad5a012761afa3f7abe83e199cbe
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:03:08 +0800

    assigned global variable and counter so user can keep track

--- practice_programs_batch_2/input_ten_numbers_print_number_of_even_numbers.py
+++ practice_programs_batch_2/input_ten_numbers_print_number_of_even_numbers.py
@@ -1,5 +1,10 @@
+global counter
+ 
 #ask user to input ten numbers
-input_number= int(input("Enter number:"))
+while counter!=10:
+    print("counter:", counter)
+    input_number= int(input("Enter number:"))
+    counter= counter+1
  
 #determine which are odd and even
 #print the number of even numbers
