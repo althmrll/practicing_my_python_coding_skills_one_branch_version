c5aa5af6639c443bab3be48b6b90985194b42171
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:39:38 +0800

    fixed the equation

--- practice_programs_batch_1/input_two_numbers_print_first_number_raised_to_the_second_number.py
+++ practice_programs_batch_1/input_two_numbers_print_first_number_raised_to_the_second_number.py
@@ -3,7 +3,7 @@ first_number = int(input("Enter first number:"))
 second_number = int(input("Enter second number:"))
 
 #Raise first number to second number
-result = first_number^second_number
+result = first_number**second_number
 
 #Print result
 print("result:", result)
