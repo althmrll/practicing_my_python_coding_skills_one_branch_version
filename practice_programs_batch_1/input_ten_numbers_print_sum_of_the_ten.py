40613a819888ce0ae117159839ed119f18982254
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:44:39 +0800

    Code to find the sum of the ten numbers

--- practice_programs_batch_1/input_ten_numbers_print_sum_of_the_ten.py
+++ practice_programs_batch_1/input_ten_numbers_print_sum_of_the_ten.py
@@ -1,4 +1,15 @@
+global input_number
+global counter
+global result
+
 #Ask user to input ten numbers
-input_number = int(input("Enter number:"))
 #Find sum of ten numbers
+counter = 0
+result = 0
+ 
+while counter != 10:
+    input_number = int(input("Enter number:"))
+    result = result + number
+    count = count + 1
+    
 # #Print sum of the ten numbers
