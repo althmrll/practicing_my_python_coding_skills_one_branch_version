38d0a04d8e6bb5074e79ba5dca4de41457a1eaff
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:23:04 +0800

    pseudocode

---
+++ practice_programs_batch_2/input_two_numbers_print_all_numbers_between_them.py
@@ -0,0 +1,4 @@
+#ask user to in put numbers
+#Add one to lower number of the two(lowernumber+1)
+#continue adding one until it reaches just below the higher number befre stopping
+#print the numbers between them
