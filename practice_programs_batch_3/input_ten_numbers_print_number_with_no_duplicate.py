2c1dd3c3d58fb4dbe687727dd76f84bf37948659
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:53:44 +0800

    used while loop that will ask user to input number until they inputted ten numbers

--- practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
+++ practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
@@ -1,3 +1,8 @@
+global counter
+ 
 #Ask user to input ten numbers
+while counter!=10:
+    number_input=int(input("Enter number:"))
+    counter=counter+1
 #Determine whether user inputted a number more than one time
 #print numbers without duplicate
