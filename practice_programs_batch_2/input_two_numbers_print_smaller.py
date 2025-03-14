afdc8921370c6b1b6092a28ca0babbdf74fce757
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 19:49:12 +0800

    wrote condition and outputs for when each number is smaller

--- practice_programs_batch_2/input_two_numbers_print_smaller.py
+++ practice_programs_batch_2/input_two_numbers_print_smaller.py
@@ -4,3 +4,8 @@ second_number=int(input("Enter first number:"))
  
 #Determine which number is smaller
 #Print the smaller number
+if first_number<second_number:
+    print(first_number) #Print the smaller number
+ 
+else: #if first_number>second_number
+    print(second_number) #Print the smaller number 
