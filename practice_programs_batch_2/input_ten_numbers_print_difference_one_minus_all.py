17b6c7c22c035d30dd7e70b4105bd20bbc2c59ae
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 20:48:41 +0800

    wrote equation for first number minus all and code so their difference will be printed

--- practice_programs_batch_2/input_ten_numbers_print_difference_one_minus_all.py
+++ practice_programs_batch_2/input_ten_numbers_print_difference_one_minus_all.py
@@ -9,7 +9,11 @@ counter= counter+1
 #Ask user to input other 9 numbers
 while counter!=10:
     other_numbers=int(input("Enter another number:"))
-    counter= counter+1
-#Find total of the other 9 numbers
+    counter= counter+1 
+    total=other_numbers+1 #Find total of the other 9 numbers
+ 
 #subtract sum of the 9 numbers to the first number
+difference= first_number-total
+ 
 #Print their difference
+print("difference:", difference)
