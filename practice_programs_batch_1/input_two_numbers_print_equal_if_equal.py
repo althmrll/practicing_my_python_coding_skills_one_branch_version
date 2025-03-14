ddbf16f6aaba5005b393b6c3267f1f66b050a504
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:02:10 +0800

    added a "Not Equal" condition

--- practice_programs_batch_1/input_two_numbers_print_equal_if_equal.py
+++ practice_programs_batch_1/input_two_numbers_print_equal_if_equal.py
@@ -1,6 +1,9 @@
 #Ask user to input two numbers
 first_number= int(input("Enter first number:"))
 second_number= int(input("Enter first number:"))
+
 #Determine if the two numbers are equal. Print "Equal" if they are equal.
 if first_number==second_number:
     print("Equal")
+else:
+    print("Not Equal")
