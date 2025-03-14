4da37770ee6d81a874fb79151ae8fb7066aad23f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 22:06:21 +0800

    added spaces and fixed condition

--- practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
+++ practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
@@ -1,13 +1,15 @@
 global counter
 inputted_number=[]
 counter = 0
+
 #Ask user to input ten numbers
 while counter!=10:
     number_input=int(input("Enter number:"))
     counter=counter+1
     inputted_number.append(number_input)
     unique_numbers=set(inputted_number)
-    if len(set)!=len(inputted_number): #Determine whether user inputted a number more than one time
+
+    if len(unique_numbers)!=len(inputted_number): #Determine whether user inputted a number more than one time
         inputted_number.remove()
  
 print(inputted_number) #print numbers without duplicate
