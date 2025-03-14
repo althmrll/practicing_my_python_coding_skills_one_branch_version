945cc8f0ca6ec18a5c27e94fdb6911b830115cba
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:37:52 +0800

    code to input the two numbers

--- practice_programs_batch_1/input_two_numbers_print_first_number_raised_to_the_second_number.py
+++ practice_programs_batch_1/input_two_numbers_print_first_number_raised_to_the_second_number.py
@@ -1,3 +1,6 @@
 #Ask user to input two numbers
+first_number = int(input("Enter first number:"))
+second_number = int(input("Enter second number:"))
+
 #Raise first number to second number
 #Print result
