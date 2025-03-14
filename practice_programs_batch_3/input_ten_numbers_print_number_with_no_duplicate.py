c574f81f5eb35b8a10a1f2998beee168fca6f118
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 22:08:33 +0800

    removed all attempt and started from scratch

--- practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
+++ practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
@@ -1,16 +1,3 @@
-global counter
-inputted_number=[]
-counter = 0
-
 #Ask user to input ten numbers
-while counter!=10:
-    number_input=int(input("Enter number:"))
-    counter=counter+1
-    
-    if len(current_numbers)!=len(inputted_number): #Determine whether user inputted a number more than one time
-        inputted_number.remove(number_input)
-    else:
-        inputted_number.append(number_input)
-        current_numbers=set(inputted_number)
- 
-print(inputted_number) #print numbers without duplicate
+#Determine whether user inputted a number more than one time
+#print numbers without duplicate
