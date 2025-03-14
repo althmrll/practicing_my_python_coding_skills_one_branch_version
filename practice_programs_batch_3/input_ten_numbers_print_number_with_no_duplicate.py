009ebea613412d1c3da151a4046568ed34780850
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:59:45 +0800

    created list and used append function so each input is added on the list

--- practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
+++ practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
@@ -1,9 +1,9 @@
 global counter
- 
+inputted_number=[]
 #Ask user to input ten numbers
 while counter!=10:
     number_input=int(input("Enter number:"))
     counter=counter+1
-
+    inputted_number.append(number_input)
 #Determine whether user inputted a number more than one time
 #print numbers without duplicate
