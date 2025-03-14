d0af2c8ca65e6ecafff2af2290cdbeffcf5ce582
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:55:50 +0800

    code to determine if number is odd and count number of odd numbers

--- practice_programs_batch_1/input_ten_numbers_print_how_many_are_odd.py
+++ practice_programs_batch_1/input_ten_numbers_print_how_many_are_odd.py
@@ -1,4 +1,16 @@
+global counter
+global odd
+ 
+counter = 0
+odd = 0
+ 
 #Ask user ot input ten numbers
-input_number = int(input("Enter number:"))
-#Determine whether the inputted number is odd
+while number != 10:
+    input_number = int(input("Enter number:"))
+    counter = counter + 1
+ 
+ #Determine whether the inputted number is odd
+    if input_number % 2 == 1:
+        odd = odd + 1
+
 #Print how many are odd
