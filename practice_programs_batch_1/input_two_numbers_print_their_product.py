f124d25ffbeaf47840225b7acf6609f7bb142bdf
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:22:54 +0800

    multiplying the two numbers to get the product

--- practice_programs_batch_1/input_two_numbers_print_their_product.py
+++ practice_programs_batch_1/input_two_numbers_print_their_product.py
@@ -3,4 +3,6 @@ first_number= int(input("Enter first number:"))
 second_number= int(input("Enter second number:")) 
  
 #Find their product
+product= first_number*second_number
+
 #Print their product
