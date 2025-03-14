9ddafbb4587da0f0348a772b671f5028839dcf01
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 20:39:26 +0800

    pseudocode

---
+++ practice_programs_batch_2/input_ten_numbers_print_difference_one_minus_all.py
@@ -0,0 +1,5 @@
+#Ask user to input first number
+ #Ask user to input other 9 numbers
+ #Find total of the other 9 numbers
+ #subtract sum of oteh 9 numbers to the first number
+ #Print their difference
