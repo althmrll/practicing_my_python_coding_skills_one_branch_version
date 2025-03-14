71fac62f37ec716fbca269430e51e7f5d2e1f098
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 20:17:27 +0800

    wrote code for the input of the two numbers

--- practice_programs_batch_2/input_two_numbers_print_their_difference.py
+++ practice_programs_batch_2/input_two_numbers_print_their_difference.py
@@ -1,3 +1,9 @@
 #Ask user to input two numbers
+first_number= int(input("Enter first number:"))
+second_number= int(input("Enter second number:"))
+ 
 #Subtract the two numbers together (1st-2nd)
+difference= first_number-second_number
+ 
 #Print their result 
+print("difference:", difference)
