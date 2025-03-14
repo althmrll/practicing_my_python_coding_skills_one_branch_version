7c8a0a6054b38b70210df1b055c10df000f188b8
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:30:51 +0800

    code so user can input two numbers

--- practice_programs_batch_2/input_two_numbers_print_all_numbers_between_them.py
+++ practice_programs_batch_2/input_two_numbers_print_all_numbers_between_them.py
@@ -1,4 +1,7 @@
-#ask user to in put numbers
+#ask user to input numbers
+lower_number= int(input("Enter lower limit:"))
+higher_number= int(input("Enter higher limit:"))
+ 
 #Add one to lower number of the two(lowernumber+1)
 #continue adding one until it reaches just below the higher number befre stopping
 #print the numbers between them
