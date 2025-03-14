cb4e49749ef049781a5f4af43132bdf2632da42d
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 17:57:37 +0800

    added spaces to make the code look more organized

--- practice_programs_batch_1/input_two_numbers_print_bigger.py
+++ practice_programs_batch_1/input_two_numbers_print_bigger.py
@@ -1,8 +1,10 @@
 #Ask user to input two number
 first_number=int(input("Enter first number:"))
 second_number=int(input("Enter second number:"))
+
 #Determine which of the two numbers are bigger
 if first_number>second_number:
     print("The bigger number is", first_number)
+    
 elif first_number<second_number:
     print("The bigger number is", second_number)
