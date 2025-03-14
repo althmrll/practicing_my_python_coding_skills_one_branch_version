f871d4c4d47c492732fba61f528e3edbc48584c9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 19:53:20 +0800

    wrote code so user can input the two numbers

--- practice_programs_batch_2/input_two_numbers_print_not_equal_if_not_equal.py
+++ practice_programs_batch_2/input_two_numbers_print_not_equal_if_not_equal.py
@@ -1,3 +1,6 @@
 #Ask user to input two numbers
+first_number= int(input("Enter first number:"))
+second_number= int(input("Enter second numebr:"))
+ 
 #determine whether the two numbers ar enot equal
 #print "not equal" if not equal
