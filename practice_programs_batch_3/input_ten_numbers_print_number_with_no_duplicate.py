bf18ba235071adfde2eee9d9d70fdc2c77b686bc
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 22:04:28 +0800

    wrote conditions for duplicated numbers to not be printed (trial)

--- practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
+++ practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
@@ -1,9 +1,13 @@
 global counter
 inputted_number=[]
+
 #Ask user to input ten numbers
 while counter!=10:
     number_input=int(input("Enter number:"))
     counter=counter+1
     inputted_number.append(number_input)
-#Determine whether user inputted a number more than one time
-#print numbers without duplicate
+    unique_numbers=set(inputted_number)
+    if len(set)!=len(inputted_number): #Determine whether user inputted a number more than one time
+        inputted_number.remove()
+ 
+print(inputted_number) #print numbers without duplicate
