97b96bd50aa9fc5edd0ab2d6e035ae0831a62128
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:15:42 +0800

    added equation to find the two numbers' sum and added spaces

--- practice_programs_batch_1/input_two_numbers_print_their_sum.py
+++ practice_programs_batch_1/input_two_numbers_print_their_sum.py
@@ -1,4 +1,6 @@
 #Ask user to input two number
 first_number = int(input("Enter first number:"))
 second_number = int(input("Enter second number:"))
+
 #Find their sum (first_number+second_number)
+sum = first_number + second_number
