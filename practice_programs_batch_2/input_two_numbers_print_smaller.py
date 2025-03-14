d7d2ea9c963ebee106a8e16d4b28d2d73452806f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 19:47:17 +0800

    wrote code so user can input number

--- practice_programs_batch_2/input_two_numbers_print_smaller.py
+++ practice_programs_batch_2/input_two_numbers_print_smaller.py
@@ -1,3 +1,6 @@
 #Ask user to input two numbers
+first_number=int(input("Enter first number:"))
+second_number=int(input("Enter first number:"))
+ 
 #Determine which number is smaller
 #Print the smaller number
