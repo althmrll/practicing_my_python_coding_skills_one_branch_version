2803fadd5d5da077cfe1b14a16bde684392efbcd
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:31:28 +0800

    equation to find their quotient

--- practice_programs_batch_1/input_two_numbers_print_their_quotient_with_decimal.py
+++ practice_programs_batch_1/input_two_numbers_print_their_quotient_with_decimal.py
@@ -3,4 +3,6 @@ first_number= int(input("Enter first number:")
 second_number= int(input("Enter second number:"))
 
 #Divide the two numbers
+result= first_number/second_number
+
 #Print their quotient(with decimal)
