51f3ebe6cb2badd2aa9c8684b8e082f509c8380e
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 18:00:39 +0800

    code in order to determine whether the two programs are equal and printing "Equal" if they are

--- practice_programs_batch_1/input_two_numbers_print_equal_if_equal.py
+++ practice_programs_batch_1/input_two_numbers_print_equal_if_equal.py
@@ -2,3 +2,5 @@
 first_number= int(input("Enter first number:"))
 second_number= int(input("Enter first number:"))
 #Determine if the two numbers are equal. Print "Equal" if they are equal.
+if first_number==second_number:
+    print("Equal")
