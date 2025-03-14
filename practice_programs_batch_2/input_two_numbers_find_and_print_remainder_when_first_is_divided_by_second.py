8bd63ce6d84c41ec116958d0976379973507b155
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 20:36:29 +0800

    fixed error in my remainder equation

--- practice_programs_batch_2/input_two_numbers_find_and_print_remainder_when_first_is_divided_by_second.py
+++ practice_programs_batch_2/input_two_numbers_find_and_print_remainder_when_first_is_divided_by_second.py
@@ -3,7 +3,7 @@ first_number= int(input("Enter first number:"))
 second_number= int(input("Enter second number:"))
  
 #Find their remainder
-remainder: first_number%second_number
+remainder= first_number%second_number
  
  # Print it
  print("remainder:", remainder)
