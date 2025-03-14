32ca375c400a91e08c037d8480e530fe79d368d1
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 21:36:59 +0800

    modified so that higher limit is not included

--- practice_programs_batch_2/input_two_numbers_print_all_numbers_between_them.py
+++ practice_programs_batch_2/input_two_numbers_print_all_numbers_between_them.py
@@ -1,10 +1,11 @@
-global middle_number
+global start
  
 #ask user to input numbers
 lower_number= int(input("Enter lower limit:"))
 higher_number= int(input("Enter higher limit:"))
+start=lower_number+1
  
 #continue adding one until it reaches just below the higher number befre stopping
-for i in range (lower_number, higher_number):
-    middle_number=i+1
-    print(middle_number) #print the numbers between them
+for i in range (start, higher_number):
+    print(i) #print the numbers between them
+    i=i+1
