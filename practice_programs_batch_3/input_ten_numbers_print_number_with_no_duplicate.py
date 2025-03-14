b0f6e5ba048a553197c2eb6c3e9fe0c3df0291b9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 22:11:02 +0800

    did the same as first attempt, but added another while loop inside of the if statement

--- practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
+++ practice_programs_batch_3/input_ten_numbers_print_number_with_no_duplicate.py
@@ -1,3 +1,19 @@
+inputted_numbers=[]
+counter= 0
+
 #Ask user to input ten numbers
+while counter!=10:
+    print("\n numbers inputted:", counter)
+    number_input= int(input("Enter number:"))
+    counter= counter+1
+    inputted_numbers.append(number_input)
+ 
 #Determine whether user inputted a number more than one time
+    if inputted_numbers.count(number_input)>1:
+        while inputted_numbers.count(number_input)>=1:
+            inputted_numbers.remove(number_input)
+    else:
+        continue
+ 
 #print numbers without duplicate
+print(inputted_numbers)
