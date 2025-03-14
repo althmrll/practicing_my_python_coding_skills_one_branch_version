740576050fba8ce446bcc2330b36b26daa08f97f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 20:22:58 +0800

    wrote initial equation and the line of code which prints their quotient

--- practice_programs_batch_2/input_two_numbers_print_quotient_without_decimal_point.py
+++ practice_programs_batch_2/input_two_numbers_print_quotient_without_decimal_point.py
@@ -3,4 +3,7 @@ first_number= int(input("Enter first numebr:"))
 second_number= int(input("Enter second number:"))
  
 #Find their quotient (1st/second)
+quotient= first_number/second_number
+ 
 #print their result
+print("quotient:", quotient)
