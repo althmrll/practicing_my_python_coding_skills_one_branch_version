4e9526a277861fa5ce950364e2398aaf05709728
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-14 20:46:49 +0800

    Write code so user can input the ten numbers

--- practice_programs_batch_2/input_ten_numbers_print_difference_one_minus_all.py
+++ practice_programs_batch_2/input_ten_numbers_print_difference_one_minus_all.py
@@ -1,5 +1,15 @@
+global counter
+global total
+ 
 #Ask user to input first number
- #Ask user to input other 9 numbers
- #Find total of the other 9 numbers
- #subtract sum of oteh 9 numbers to the first number
- #Print their difference
+counter= 0
+first_number= int(input("Enter number"))
+counter= counter+1
+ 
+#Ask user to input other 9 numbers
+while counter!=10:
+    other_numbers=int(input("Enter another number:"))
+    counter= counter+1
+#Find total of the other 9 numbers
+#subtract sum of the 9 numbers to the first number
+#Print their difference
