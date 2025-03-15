256e841d3877f5f7b119c3ed6de956f4f690947e
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:00:08 +0800

    added comments to note what each line of code does

--- practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
+++ practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
@@ -7,12 +7,11 @@ while True: #determine whether input is invalid
         inputted_number.append(number)
         filtered_list=set(inputted_number)
  
-        if len(inputted_number)==len(filtered_list):
-            print("Unique")
+        if len(inputted_number)==len(filtered_list): #determines whether the number is unique or a duplicate
+            print("Unique") #displays when the number is unique
         else:
-            print("Duplicate")
-         
+            print("Duplicate") #displays when the number is a duplicate
+ 
     except ValueError:
         print("!!INVALID INPUT!!")
         break
-#display whether the number is unique or a duplicate
