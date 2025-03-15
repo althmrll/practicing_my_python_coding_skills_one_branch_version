eb711089d651b0dca48333ac6afee4a173a900cc
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:13:42 +0800

    fixed typos

--- practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
+++ practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
@@ -4,11 +4,11 @@ inputted_number=[]
 #Ask user ot input number until invalid
 while True:
     try:
-        number_input= int(inout("Enter number:"))
+        number_input= int(input("Enter number:"))
         count=count+1
         inputted_number.append(number_input)
 
 #print numbers in descending order
     except ValueError:
-        inputted_number(sort(reversed=True)) #sorts number into descending order
+        inputted_number.sort(reverse=True) #sorts number into descending order
         print(inputted_number) #prints the numbers
