6f2da6c827541e662993024597c91ac48fa27c60
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:28:59 +0800

    added input with limit of ten and the list where each inputted number will be stored

--- practice_programs_batch_4/input_ten_numbers_display_numbers_with_duplicate.py
+++ practice_programs_batch_4/input_ten_numbers_display_numbers_with_duplicate.py
@@ -1,3 +1,12 @@
+counter= 0
+inputted_numbers= []
+
 #ask user to input ten numbers
+while counter!=10:
+    print("numbers inputted:", counter)
+    number_input= int(input("Enter number:"))
+    counter=counter+1
+    inputted_numbers.append(number_input)
+ 
 #determine which inputs have duplicates
 #print all numbers that have duplicates
