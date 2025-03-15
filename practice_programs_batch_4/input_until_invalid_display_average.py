b2ed03ac0fdcc4c453df5a007b01bf75103c011a
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:17:36 +0800

    pseudocode

---
+++ practice_programs_batch_4/input_until_invalid_display_average.py
@@ -0,0 +1,6 @@
+#Ask user to input number continuously
+# Find their average
+    #Add all the numbers together
+    #Count the number of numbers inputted
+    #Divide the total of numbers to the count of numbers inputted
+#Print their average
