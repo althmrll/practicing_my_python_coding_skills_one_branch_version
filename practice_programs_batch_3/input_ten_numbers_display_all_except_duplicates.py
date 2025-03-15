cc21d04ca73487bc2a7fcfb608734e44ac2d86e2
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 09:41:16 +0800

    used while loop so program continues to ask for input until user types in an invalid one

--- practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
+++ practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
@@ -1,2 +1,8 @@
 #input ten numbers
+while True: #determines whether input is invalid
+    try:
+        number=int(input("Enter number:"))
+     
+    except ValueError:
+        break
 #print all inputted numbers except duplicates
