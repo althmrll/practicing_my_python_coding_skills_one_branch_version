aaebc578a071a7132bc229daa5e2a156efc6e2e0
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:45:33 +0800

    edited pseudocode and wrote code to determine which duplicate count is highest

--- practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
+++ practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
@@ -1,9 +1,17 @@
 inputted_numbers=[]
+highest_duplicate_count=0
 
 #Ask user to input number until invalid number.
 while True:
-    number_input= int(input("Enter number:"))
-    inputted_numbers.append(number_input)
+    try:
+        number_input= int(input("Enter number:"))
+        inputted_numbers.append(number_input)
+ 
+ #Determine which numbers are duplicates and the highets duplicate count
+        inputted_numbers.count (number_input)
+        if inputted_numbers.count (number_input)>highest_duplicate_count:
+            highest_duplicate_count= number_input
+        else:
+            continue
 
-#Determine which numbers are dupliates
 #Print number with the most duplicates.
