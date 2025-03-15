f8d09a04461bac928bfe9814a2f3d583370b6032
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 09:56:08 +0800

    added list and used append so each input is added on list. Also added error message before program breaks

--- practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
+++ practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
@@ -1,9 +1,12 @@
+inputted_number= []
+ 
 #input number(use while loop)
-while True:
+while True: #determine whether input is invalid
     try:
         number= int(input("Enter number:"))
+        inputted_number.append(number)
          
     except ValueError:
+        print("!!INVALID INPUT!!")
         break
 #display whether the number is unique or a duplicate
-#determine whether input is invalid
