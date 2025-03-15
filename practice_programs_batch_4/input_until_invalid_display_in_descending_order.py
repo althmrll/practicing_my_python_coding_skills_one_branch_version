a9e3a3f223ded3c579e78da80cde291a5fbe37b6
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:10:57 +0800

    used while so user can input until invalid. Also added counter for user and lists so each input is recorded

--- practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
+++ practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
@@ -1,2 +1,10 @@
+count=0
+inputted_number=[]
+
 #Ask user ot input number until invalid
+while True:
+    try:
+        number_input= int(inout("Enter number:"))
+        count=count+1
+        inputted_number.append(number_input)
 #print numbers in descending order
