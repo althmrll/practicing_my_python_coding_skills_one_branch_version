d4156ddfa129d0efd0f62175985bec2719a03379
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:14:55 +0800

    removed count variable

--- practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
+++ practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
@@ -1,11 +1,9 @@
-count=0
 inputted_number=[]
 
 #Ask user ot input number until invalid
 while True:
     try:
         number_input= int(input("Enter number:"))
-        count=count+1
         inputted_number.append(number_input)
 
 #print numbers in descending order
