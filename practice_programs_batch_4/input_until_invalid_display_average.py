a586561af4e1dce48539c6e4c39707c066cd04eb
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:19:02 +0800

    assigned values, wrote code for inputting the numbers, and wrote inittial equation for finding sum of all the inputted numbers

--- practice_programs_batch_4/input_until_invalid_display_average.py
+++ practice_programs_batch_4/input_until_invalid_display_average.py
@@ -1,6 +1,13 @@
+count=0
+total=0
+
 #Ask user to input number continuously
+while True:
+    number= int(input("Enter number:"))
 # Find their average
     #Add all the numbers together
+    total= total+number
     #Count the number of numbers inputted
+    count=count+1
     #Divide the total of numbers to the count of numbers inputted
 #Print their average
