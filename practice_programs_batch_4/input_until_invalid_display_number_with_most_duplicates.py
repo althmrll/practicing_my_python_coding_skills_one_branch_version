8f00fcf237be2ee7076eb7185183b3e5e02d164c
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:43:05 +0800

    added input so user can add number until invalid. Also added list to record all inputted numbers

--- practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
+++ practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
@@ -1,3 +1,9 @@
+inputted_numbers=[]
+
 #Ask user to input number until invalid number.
+while True:
+    number_input= int(input("Enter number:"))
+    inputted_numbers.append(number_input)
+
 #Determine which numbers are dupliates
 #Print number with the most duplicates.
