cb6ac00f6980d99649b5d9d418931d25bc79fbc6
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 09:49:36 +0800

    undid all work after creation of list and used a set for the list with only unique numbers

--- practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
+++ practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
@@ -5,11 +5,8 @@ while True: #determines whether input is invalid
     try:
         number=int(input("Enter number:"))
         inputted_number.append(number)#adds each input on the list
-        inputted_number.count(number) #determines whether a value has repeated
- 
-        if inputted_number.count(number)>1:
-            inputted_number.remove #print all inputted numbers except duplicates
+        filtered_list=set(inputted_number) #creates set that excludes all duplicates
      
     except ValueError:
-        print(inputted_number)
+        print(filtered_list) #prints the set
         break
