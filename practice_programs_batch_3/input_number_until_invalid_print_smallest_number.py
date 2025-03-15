8f00ebd2832c34782f4cc2fe42f220a5f6464148
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:06:11 +0800

    added print function and specified what item to print on the list

--- practice_programs_batch_3/input_number_until_invalid_print_smallest_number.py
+++ practice_programs_batch_3/input_number_until_invalid_print_smallest_number.py
@@ -9,6 +9,7 @@ while True: #ensures the program continuously asks for input that stops when inp
         inputted_number.append(number)
          
     except ValueError:
+        #print smallest number
         inputted_number.sort() #sorts list in ascending order
+        print(inputted_number[0]) #prints first item on the list
         break
-#print smallest number
