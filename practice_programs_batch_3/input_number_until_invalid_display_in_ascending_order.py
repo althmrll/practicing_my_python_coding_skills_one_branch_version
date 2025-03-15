90cb05349f7175b4fbf03df84def2b73908ce044
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:14:53 +0800

    added a list wherein every input is added into until the user types an invalid input

--- practice_programs_batch_3/input_number_until_invalid_display_in_ascending_order.py
+++ practice_programs_batch_3/input_number_until_invalid_display_in_ascending_order.py
@@ -1,7 +1,10 @@
+inputted_number=[]
+ 
 #input number(use while loop)
 while True: #determines whether input is invalid
     try:
         number=int(input("Enter number:"))
+        inputted_number.append(number)
      
     except ValueError:
         break
