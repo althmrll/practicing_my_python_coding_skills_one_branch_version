68892823e2df18e48e56b20f82c7944b5059634d
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:06:06 +0800

    used while so programs continuously asks users number until invalid. Also wrote code that prints list after invalid input

--- practice_programs_batch_4/input_until_invalid_display_highest_number.py
+++ practice_programs_batch_4/input_until_invalid_display_highest_number.py
@@ -1,2 +1,11 @@
+numbers_inputted=[]
+
 #Ask user ot input number until invalid
+while True:
+    try:
+        number_input= int(input("Enter number:"))
+        numbers_inputted.append(number_input)
+    except ValueError:
+        print(numbers_inputted)
+ 
 #Print highest number
