1151e05014748f909cb513b030da7e737216d925
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:00:48 +0800

    edited condition so that count of the number with most duplicate and number number input is compared

--- practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
+++ practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
@@ -2,14 +2,16 @@ inputted_numbers=[]
 most_duplicated=0
 #Ask user to input number until invalid number.
 while True:
+    most_duplicated=inputted_numbers.count (number_input)
     try:
         number_input= int(input("Enter number:"))
         inputted_numbers.append(number_input)
  
  #Determine which numbers are duplicates and the highets duplicate count
         inputted_numbers.count (number_input)
-        if inputted_numbers.count(number_input)>most_duplicated:
-             most_duplicated= number_input
+        print(inputted_numbers.count (number_input))
+        if inputted_numbers.count(number_input)>inputted_numbers.count(most_duplicated):
+            most_duplicated= number_input
         else:
             continue
 
