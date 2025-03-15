f92b3c624f7ab90b84203b0c2ec4cb4e05780651
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:04:48 +0800

    added list and used append action so each input is added on the list. Also added sort so the list is sorted in ascending order

--- practice_programs_batch_3/input_number_until_invalid_print_smallest_number.py
+++ practice_programs_batch_3/input_number_until_invalid_print_smallest_number.py
@@ -1,10 +1,14 @@
+inputted_number=[]
+
 #input number(use while loop)
 #determine whether input is invalid
 
 while True: #ensures the program continuously asks for input that stops when input is invalid
     try:
         number= int(input("Enter number:"))
+        inputted_number.append(number)
          
     except ValueError:
+        inputted_number.sort() #sorts list in ascending order
         break
 #print smallest number
