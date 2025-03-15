7fcf283f3720170125d80d5e6cec57ab8acbafb2
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 09:43:51 +0800

    Used count function to count occurences of each value. Also added a print function for when an invalid input is typed

--- practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
+++ practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
@@ -4,8 +4,11 @@ inputted_number=[]
 while True: #determines whether input is invalid
     try:
         number=int(input("Enter number:"))
-        inputted_number.append(number)#adds input on the list
+        inputted_number.append(number)#adds each input on the list
+        inputted_number.count #determines whether a value has repeated
      
     except ValueError:
+        print(inputted_number)
         break
+
 #print all inputted numbers except duplicates
