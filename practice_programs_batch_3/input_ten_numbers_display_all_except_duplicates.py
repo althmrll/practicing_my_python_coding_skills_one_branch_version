b37da6c93dba5f0bf21bee90ec33879c38c63da9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 09:47:24 +0800

    added argument on the list count to fix error

--- practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
+++ practice_programs_batch_3/input_ten_numbers_display_all_except_duplicates.py
@@ -5,9 +5,9 @@ while True: #determines whether input is invalid
     try:
         number=int(input("Enter number:"))
         inputted_number.append(number)#adds each input on the list
-        inputted_number.count() #determines whether a value has repeated
+        inputted_number.count(number) #determines whether a value has repeated
  
-        if inputted_number.count()>1:
+        if inputted_number.count(number)>1:
             inputted_number.remove #print all inputted numbers except duplicates
      
     except ValueError:
