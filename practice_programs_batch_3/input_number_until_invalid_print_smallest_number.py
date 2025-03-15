bac9ceee5171a594a0a18db5a72b2669bdd6e7bf
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 10:03:20 +0800

    used while loop so program asks continuously for input until faced with an invalid input

--- practice_programs_batch_3/input_number_until_invalid_print_smallest_number.py
+++ practice_programs_batch_3/input_number_until_invalid_print_smallest_number.py
@@ -1,3 +1,10 @@
 #input number(use while loop)
 #determine whether input is invalid
+
+while True: #ensures the program continuously asks for input that stops when input is invalid
+    try:
+        number= int(input("Enter number:"))
+         
+    except ValueError:
+        break
 #print smallest number
