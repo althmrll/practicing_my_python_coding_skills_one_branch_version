c2be9851c89358d397d96aa0a225f512e1cbf588
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:02:29 +0800

    removed some misplaced lines of coding and added finishing touches (spaces for when the code is run)

--- practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
+++ practice_programs_batch_4/input_until_invalid_display_number_with_most_duplicates.py
@@ -2,14 +2,12 @@ inputted_numbers=[]
 most_duplicated=0
 #Ask user to input number until invalid number.
 while True:
-    most_duplicated=inputted_numbers.count (number_input)
     try:
         number_input= int(input("Enter number:"))
         inputted_numbers.append(number_input)
  
  #Determine which numbers are duplicates and the highets duplicate count
         inputted_numbers.count (number_input)
-        print(inputted_numbers.count (number_input))
         if inputted_numbers.count(number_input)>inputted_numbers.count(most_duplicated):
             most_duplicated= number_input
         else:
@@ -17,4 +15,4 @@ while True:
 
 #Print number with the most duplicates.
     except ValueError:
-        print ("The most duplicated number is", most_duplicated)
+        print("\nThe most duplicated number is", most_duplicated)
