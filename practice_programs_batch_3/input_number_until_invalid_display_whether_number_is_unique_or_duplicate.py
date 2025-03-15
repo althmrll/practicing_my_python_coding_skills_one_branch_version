1077f9d70e5d67e92dc2acc8bde0052c6fe64bba
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 09:53:01 +0800

    wrote code so program continuously asks for input until an invalid value is typed in

--- practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
+++ practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
@@ -1,3 +1,9 @@
 #input number(use while loop)
+while True:
+    try:
+        number= int(input("Enter number:"))
+         
+    except ValueError:
+        break
 #display whether the number is unique or a duplicate
 #determine whether input is invalid
