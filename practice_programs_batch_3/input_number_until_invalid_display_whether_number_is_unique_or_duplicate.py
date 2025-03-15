b8577403e29de202e9f16a71ac798de0a414c57a
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 09:57:28 +0800

    added condition to determine if number is unique or not

--- practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
+++ practice_programs_batch_3/input_number_until_invalid_display_whether_number_is_unique_or_duplicate.py
@@ -5,6 +5,12 @@ while True: #determine whether input is invalid
     try:
         number= int(input("Enter number:"))
         inputted_number.append(number)
+        filtered_list=set(inputted_number)
+ 
+        if number!=filtered_list:
+            print("Unique")
+        else:
+            print("Duplicate")
          
     except ValueError:
         print("!!INVALID INPUT!!")
