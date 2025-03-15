ce74d1f4b4a55edf064314094e88b050ed590229
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-15 11:12:28 +0800

    used sort to sort numbers in descending order. Also used print function to print sorted list

--- practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
+++ practice_programs_batch_4/input_until_invalid_display_in_descending_order.py
@@ -7,4 +7,8 @@ while True:
         number_input= int(inout("Enter number:"))
         count=count+1
         inputted_number.append(number_input)
+
 #print numbers in descending order
+    except ValueError:
+        inputted_number(sort(reversed=True)) #sorts number into descending order
+        print(inputted_number) #prints the numbers
